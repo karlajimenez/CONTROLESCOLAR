f9b615f48ac262e5a24503fb720115f275427af1
VicTravieZzo <ti1111526m@gmail.com>
2014-12-05 23:06:29 -0600

    CONTROL ESCOLAR
    CATALOGO DE BAJAS ALTAS Y MODIFICACIONES DE USUARIOS
    LOGIN DE ACCESO
    Y MODULO DE ASIGNACION DE MATERIAS A PROFESORES Y ALUMNOS A GRUPOS

---
+++ menu.php
@@ -0,0 +1,32 @@
+
+
+
+<div class="masthead">
+
+            <h3 class="text-muted" href="#">SISTEMA ESCOLAR</h3>
+        <ul class="nav nav-justified">
+
+                <li h3><a href="index.php">Inicio</a></li>
+                <li><a href="TestUsuario.php">Administraci&oacute;n de Usuarios</a></li>
+
+                <li><a href="TestMateria.php">Asignar Materias a Maestro(s)</a></li>
+                <li><a href="TestAlumno.php">Asignar Alumnos a Grupos</a></li>
+                <li><a href="cerrarsesion.php">Cerrar Sesi&oacute;n</a></li>
+                <!--<li><a href="TestAlumno.php">Alumnos</a></li>
+                <li><a href="TestMaestro.php">Administraci&oacute;n Maestros</a></li>
+                <li><a href="#">Grupos</a></li>
+                <li><a href="#">Materias</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>-->
+            </ul>
+</div>
